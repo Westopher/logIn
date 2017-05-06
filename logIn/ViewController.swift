be06c8422aa8daf4e4a9a1125d0626305eb6363b
West Kraemer <WestKraemer@gmail.com>
2017-05-06 16:57:04 -0400

    add ability for user to sign in if their name is already saved in core data under entity named 'name'

--- logIn/ViewController.swift
+++ logIn/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
@@ -24,6 +25,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //To access the functions in the AppDelegate, which allow you to use core data
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        
+        //If the user name is in the core data, then change the alphas of the text field and login button so that they disappear. Then, change the label's alpha so that it displays and shows the text below."
+        do {
+            
+            let results = try context.fetch(request)
+            
+            for result in results as! [NSManagedObject] {
+                
+                if let username = result.value(forKey: "name") as? String {
+                    
+                    textField.alpha = 0
+                    
+                    logInButton.alpha = 0
+                    
+                    label.alpha = 1
+                    
+                    label.text = "Hi there " + username + "!"
+                    
+                }
+            }
+            
+            
+        } catch {
+            
+            print("Request failed")
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
